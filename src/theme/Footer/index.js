1c3517619d6f9cdd4ebeb30dd7172ab60853984d
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-air.lan>
2023-05-16 09:24:19 -0500

    adds chat back

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Footer from '@theme-original/Footer';
 import Parser from '@site/src/components/Parser';
+import ChatBox from '@site/src/components/DocChat';
+
 
 export default function FooterWrapper(props) {
   return (
     <>
+      <ChatBox messages={[]} />
       <Footer {...props} />
     </>
   );
