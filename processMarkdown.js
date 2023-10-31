8ffc8c4c4c3d90bd6a55bf5f222d3bf2ccc01b56
avb-dev-docs <123575048+avb-dev-docs@users.noreply.github.com>
2023-10-31 10:00:45 -0700

    Create processMarkdown.js

---
+++ processMarkdown.js
@@ -0,0 +1,61 @@
+const https = require('https');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const API_KEY = process.env.API_KEY;
+const ENDPOINT_URL = new URL(process.env.ENDPOINT_URL);
+
+const getModifiedMarkdownFiles = () => {
+  const output = execSync('git diff --name-only HEAD HEAD~1').toString();
+  return output.split('\n').filter(file => file.endsWith('.md'));
+};
+
+const sendRequest = (file, content) => {
+  return new Promise((resolve, reject) => {
+    const data = JSON.stringify({ content, filepath: file });
+
+    const options = {
+      hostname: ENDPOINT_URL.hostname,
+      port: ENDPOINT_URL.port,
+      path: ENDPOINT_URL.pathname,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-key': API_KEY,
+        'Content-Length': Buffer.byteLength(data)
+      }
+    };
+
+    const req = https.request(options, res => {
+      res.setEncoding('utf8');
+      res.on('data', d => {
+        process.stdout.write(d);
+      });
+      res.on('end', resolve);
+    });
+
+    req.on('error', error => {
+      console.error(error);
+      reject(error);
+    });
+
+    req.write(data);
+    req.end();
+  });
+};
+
+const main = async () => {
+  const files = getModifiedMarkdownFiles();
+  for (const file of files) {
+    if (file) {
+      const content = fs.readFileSync(file, 'utf8');
+      try {
+        await sendRequest(file, content);
+      } catch (error) {
+        console.error(`Error processing file ${file}:`, error);
+      }
+    }
+  }
+};
+
+main();
