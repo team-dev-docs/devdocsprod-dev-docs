5dd7359170aec29c4dc614e7f190fde34b7af95d
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2023-12-03 03:50:52 +0000

    adds the click on the title

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -2,9 +2,42 @@ import React from 'react';
 import Footer from '@theme-original/Footer';
 import Parser from '@site/src/components/OmniParser';
 import ChatBox from '@site/src/components/DocChat';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
+
 
 
 export default function FooterWrapper(props) {
+  let location = useLocation();
+  useEffect(() => {
+    const tocLinks = document.querySelectorAll('.toc-highlight');
+  
+    const handleClick = (event) => {
+      // Prevent default anchor click behavior
+      // event.preventDefault();
+  
+      // Remove the active class from all TOC links
+      tocLinks.forEach(link => {
+        link.classList.remove('table-of-contents__link--active');
+      });
+  
+      // Add the active class to the clicked TOC link
+  
+      event.currentTarget.classList.add('table-of-contents__link--active');
+    };
+  
+    tocLinks.forEach(link => {
+      link.addEventListener('click', handleClick);
+    });
+  
+    // Cleanup function to remove event listener when the component unmounts
+    return () => {
+      tocLinks.forEach(link => {
+        link.removeEventListener('click', handleClick);
+      });
+    };
+  }, [location.pathname]);
   return (
     <>
       <ChatBox messages={[]} />
