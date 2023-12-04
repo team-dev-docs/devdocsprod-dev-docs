4f843901b0b0597ec505e13f50979e49924a1a43
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2023-12-04 13:41:15 +0000

    keep api docs default for now

--- dev-docs-openapi.js
+++ dev-docs-openapi.js
@@ -1,35 +1,5 @@
 const apiConfig = {
-   "users": {
-      "specPath": "examples/openapi.yaml",
-      "outputDir": "docs/api/general/users",
-      "markdownGenerators": {
-         "createInfoPageMD": function(pageMetadata) {
-            return `# Whats up! \\n${pageMetadata}`
-         },
-         "createApiPageMD": function(apiJSONData) {
-            // Adjust the import path based on your project structure
-            return `
-import ApiTabs from "@theme/ApiTabs";
-import DiscriminatorTabs from "@theme/DiscriminatorTabs";
-import MethodEndpoint from "@theme/ApiExplorer/MethodEndpoint";
-import SecuritySchemes from "@theme/ApiExplorer/SecuritySchemes";
-import MimeTabs from "@theme/MimeTabs";
-import ParamsItem from "@theme/ParamsItem";
-import ResponseSamples from "@theme/ResponseSamples";
-import SchemaItem from "@theme/SchemaItem";
-import SchemaTabs from "@theme/SchemaTabs";
-import JsonToTable from '@site/src/components/JsonToTable';
-          
 
-          
-This page is generated from JSON data.
-          
-<JsonToTable />
-
-            `;
-          }
-      }
-   }
 }
 
 module.exports = {
