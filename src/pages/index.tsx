2965006298fb9e10e1952dd9f8a8965acfff2d1a
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-2.local>
2024-01-14 08:20:20 -0800

    gets rid of landing page for now

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,23 +7,33 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Editor from '@site/src/components/editor';
 import ChatBox from '@site/src/components/ChatBox';
 import landingJson from '../../landing.json'
-import Landing from "./_landing_page.md"
-import styles from './index.module.css';
+
 import Parser from '../components/Parser';
+import {Redirect} from '@docusaurus/router';
 
-
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/intro">
+            Lets Get Started - 5min ⏱️
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
 
 export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext()
   const data = landingJson
   return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <main className="landing text-center mt-6">
-        <Landing />
-        <Parser />
-      </main>
-    </Layout>
+    <Redirect to="/docs" />
   );
 }
