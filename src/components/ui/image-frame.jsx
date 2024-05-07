3d76eaf3d52bc4a48b0f99cc5e22bbeaeb6b40ab
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-3.local>
2024-05-07 10:50:57 -0700

    adds zoom images

---
+++ src/components/ui/image-frame.jsx
@@ -0,0 +1,64 @@
+import React, { useState, useRef, useEffect } from "react";
+import { cn } from "@site/src/utils";
+
+const ImageFrame = (props) => {
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "Escape") {
+        exitFullScreen();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const enterFullScreen = () => {
+    if (imageRef.current) {
+      if (imageRef.current.requestFullscreen) {
+        imageRef.current.requestFullscreen();
+      } else if (imageRef.current.webkitRequestFullscreen) {
+        imageRef.current.webkitRequestFullscreen();
+      } else if (imageRef.current.msRequestFullscreen) {
+        imageRef.current.msRequestFullscreen();
+      }
+      setIsFullScreen(true);
+    }
+  };
+
+  const exitFullScreen = () => {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) {
+      document.msExitFullscreen();
+    }
+    setIsFullScreen(false);
+  };
+
+  const toggleFullScreen = () => {
+    if (isFullScreen) {
+      exitFullScreen();
+    } else {
+      enterFullScreen();
+    }
+  };
+
+  return (
+    <img
+      ref={imageRef}
+      {...props}
+      onClick={toggleFullScreen}
+      className={cn("w-full cursor-zoom-in", props.className)}
+    />
+  );
+};
+
+export { ImageFrame };
---
+++ src/theme/MDXComponents/Img/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
+import { ImageFrame } from '@site/src/components/ui/image-frame.jsx';
+
+function transformImgClassName(className) {
+  return clsx(className, styles.img);
+}
+
+export default function MDXImg(props) {
+  console.log("what are the props", props)
+  return (
+    <ImageFrame
+      {...props}
+    />
+  );
+}
