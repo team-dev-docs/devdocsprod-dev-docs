50b6d05174f19e3a69fbb657de6343017b9cfe22
andrew.ryan.vanbeek@gmail.com <andrewvanbeek@andrews-MacBook-Air-3.local>
2024-06-27 10:06:08 -0700

    fixes copy for safari

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -83,7 +83,7 @@ const config = {
           docItemComponent: "@theme/ApiItem",
           docLayoutComponent: "@theme/DocPage",
           sidebarPath: require.resolve('./sidebars.js'),
-          sidebarCollapsed: true,
+          sidebarCollapsed: false,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
