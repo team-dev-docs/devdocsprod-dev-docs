eadc45c1b2c798172570d9aef3541f7270472948
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-08-06 14:33:04 -0700

    added dev-docs-openapi.js

--- dev-docs-openapi.js
+++ dev-docs-openapi.js
@@ -1,7 +1,10 @@
 const apiConfig = {
-
+    "cool": {
+        "specPath": "examples/cool.yaml",
+        "outputDir": "docs/api/cool"
+    }
 }
 
 module.exports = {
-   config: apiConfig
+  config: apiConfig
 }
