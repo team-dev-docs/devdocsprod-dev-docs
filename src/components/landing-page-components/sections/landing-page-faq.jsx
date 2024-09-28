7e4df59892fef47b9f85f577a17b10426910bb49
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-09-28 16:27:16 +0000

    adds faqs

--- src/components/landing-page-components/sections/landing-page-faq.jsx
+++ src/components/landing-page-components/sections/landing-page-faq.jsx
@@ -5,16 +5,16 @@ import { useIsMobile } from '../context-providers/mobile-context-provider';
 
 const FAQS = [
   {
-    title: "Lore ipsum dolor sit amet?",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, gravida nunc. Nullam auctor, nisl id tincidunt efficitur, nulla urna vehicula turpis, nec ultrices purus purus nec nunc. Nulla nec purus feugiat, molestie ipsum et, gravida nunc. Nullam auctor, nisl id tincidunt efficitur, nulla urna vehicula turpis, nec ultrices purus purus nec nunc.",
+    title: "How does pricing work?",
+    description: "We charge a flat fee for an allotted amount of monthly credits. If you run out of credits on a given month you can always purchase more but you will never be charged overages or anything of the sort. You can always upgrade your plan for a larger monthly allotment if you are consistently reaching your monthly threshold.",
   },
   {
-    title: "Lore ipsum dolor sit amet?",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, gravida nunc. Nullam auctor, nisl id tincidunt efficitur, nulla urna vehicula turpis, nec ultrices purus purus nec nunc. Nulla nec purus feugiat, molestie ipsum et, gravida nunc. Nullam auctor, nisl id tincidunt efficitur, nulla urna vehicula turpis, nec ultrices purus purus nec nunc.",
+    title: "What are Dev-Docs’ security practices?",
+    description: "With founders coming from Okta and Stripe, Dev-Docs makes security the utmost priority in everything we do and has built the product from the ground up with a security first mindset. We believe that our users' security and privacy is paramount to our success. Your source code is your IP and should only be accessed by you. We understand the importance of protecting your IP and do not, and never will, store or train on any of your code. We are actively pursuing SOC 2 Type 2 compliance and have controls in place to do so. ",
   },
   {
-    title: "Lore ipsum dolor sit amet?",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla nec purus feugiat, molestie ipsum et, gravida nunc. Nullam auctor, nisl id tincidunt efficitur, nulla urna vehicula turpis, nec ultrices purus purus nec nunc. Nulla nec purus feugiat, molestie ipsum et, gravida nunc. Nullam auctor, nisl id tincidunt efficitur, nulla urna vehicula turpis, nec ultrices purus purus nec nunc.",
+    title: "Who is Dev-Docs for?",
+    description: "Dev-Docs is for anyone who wants to generate content from their codebase or more efficiently generate documentation. We often find developers, technical writers, and product managers use us the most. Our goal is not to replace anyone but instead to give these folks super powers. That is, we want to automate the annoying elements of their jobs so they can focus on the more strategic and fun parts!",
   },
 ];
 
