f6e6a72f66cb4f3eb25639fe9f7252edcf9b1544
avb-is-me <104213687+avb-is-me@users.noreply.github.com>
2024-09-29 03:13:17 +0000

    adss pricing

--- src/components/landing-page-components/parts/pricing-card.jsx
+++ src/components/landing-page-components/parts/pricing-card.jsx
@@ -26,6 +26,7 @@ const PricingCard = ({
         background: "linear-gradient(180deg, rgba(255, 255, 255, 0.05) 0%, rgba(255, 255, 255, 0.00) 100%)",
         border: "2px solid rgba(255, 255, 255, 0.20)",
         boxShadow: "0px -2px 10px 0px rgba(233, 223, 255, 0.30), 0px -2px 40px 0px rgba(187, 155, 255, 0.15)",
+        height: "100%"
       }}
     >
       <div
--- src/pages/pricing/index.jsx
+++ src/pages/pricing/index.jsx
@@ -6,39 +6,8 @@ import LandingPageHeader from '../../components/landing-page-components/sections
 import LandingPageFaqCard from '../../components/landing-page-components/sections/landing-page-faq';
 import LandingPageCard from '../../components/landing-page-components/parts/landing-page-card';
 import LandingPageFeatureCard from '../../components/landing-page-components/parts/pricing-card';
+import PricingCards from '@site/src/components/landing-page-components/sections/pricing-cards';
 
-
-
-
-const FEATURES = [
-  {
-    title: "Byte Plan (free tier)",
-    description: "Everything you need to get started with Dev-Docs",
-    bulletPoints: [
-      "No cc required",
-      "Access to Dev-Docs' native web app features",
-      "Use Dev-Docs in VS Code or GitHub Codespaces",
-      "Generate the content you need from your codebase",
-      "Leverage Dev-Docs Chrome Extension to generate step by step guides",
-      "Get 2,000,000 input credits and 500,000 output credits to use as you see fit",
-    ],
-    buttonTitle: "Get started",
-    href: "https://docs.dev/docs"
-  },
-  {
-    title: "Megabyte Plan (pro tier)",
-    description: "Get even more out of Dev-Docs",
-    bulletPoints: [
-      "Everything from the free plan",
-      "Starting with 10,000,000 input credits and 2,000,000 output credits per month",
-      "Integrate Dev-Docs into your CI/CD to fully automate the process",
-      "Sync Dev-Docs up with your Confluence, Google Drive or Notion",
-      "Dedicated support" 
-    ],
-    buttonTitle: "Get In Touch",
-    href: "https://dub.sh/devdocs"
-  }
-];
 
 
 const PricingPage = () => {
@@ -47,42 +16,11 @@ const PricingPage = () => {
   return (
     <MobileContextProvider>
       <div
-        className={isMobile ?
-          "w-full font-sans bg-repeat-y bg-cover bg-landing-page text-[#ECECEC] gap-[4rem] flex flex-col"
-          :
-          "w-full font-sans bg-repeat-y bg-cover bg-landing-page text-[#ECECEC] gap-[15.62rem] flex flex-col"
-        }
+        className={`w-full font-sans bg-repeat-y bg-cover bg-landing-page text-[#ECECEC] flex flex-col ${isMobile ? 'gap-4' : 'gap-[15.62rem]'}`}
       >
         <LandingPageHeader />
 
-        <div
-        className={isMobile ?
-          "mt-[3rem] flex justify-center items-center gap-[1rem]"
-          :
-          "mt-[3rem] flex flex-col flex-wrap gap-[1.25rem] justify-center w-full"
-        }
-        style={{"alignItems": "stretch"}}
-      >
-        {FEATURES.map((feature, idx) => (
-          <div
-            key={`landing-page-features-card-feature-card-${idx}`}
-            className={isMobile ?
-              "flex flex-row justify-center align-items-stretch"
-              :
-              "flex flex-row justify-center h-[23rem] align-items-stretch"
-            }
-          >
-            <LandingPageFeatureCard
-              iconSrc={feature.iconSrc}
-              title={feature.title}
-              description={feature.description}
-              bulletPoints={feature.bulletPoints}
-              href={feature.href}
-              buttonTitle={feature.buttonTitle}
-            />
-          </div>
-        ))}
-      </div>
+        <PricingCards />
 
         <LandingPageFaqCard />
         <LandingPageFooter />
